e3550adcd1e8286da988cdbd046c2e77232031d1
kmakma <michaelo1@gmx.de>
2019-12-04 13:35:17 +0100

    y2019d01 finished

--- src/main/kotlin/io/github/kmakma/adventofcode/y2019/Y2019Day01.kt
+++ src/main/kotlin/io/github/kmakma/adventofcode/y2019/Y2019Day01.kt
@@ -2,25 +2,46 @@ package io.github.kmakma.adventofcode.y2019
 
 class Y2019Day01 : Y2019Day(
     1,
-    "Result (index = 0) for intcode program with set parameter:",
-    "Parameter for intcode program with set target result:"
+    "Fuel for all modules:",
+    "Fuel for all modules and for the fuel itself:"
 ) {
     private lateinit var fuelForModules: List<Int>
 
     override fun solve() {
-        resultTask1 = calculateFuelForAllModules()
+        fuelForModules = calculateFuelForModules(getInput())
+        resultTask1 = fuelForAllModules()
+        resultTask2 = totalFuel()
     }
 
-    override fun getInput(): List<String> {
-        return linesToList()
+    override fun getInput(): List<String> = linesToList()
+
+    private fun calculateFuelForModules(modules: List<String>): List<Int> {
+        return modules
+            .map { it.toInt() }
+            .map { fuelForWeight(it) }
     }
 
-    private fun calculateFuelForAllModules(): Int {
-        fuelForModules = getInput().map { it.toInt() }.map { fuelForWeight(it) }
+    private fun fuelForAllModules(): Int {
         return fuelForModules.sum()
+    }
+
+    private fun totalFuel(): Int {
+        return fuelForModules
+            .map { it + fuelForFuel(it) }
+            .sum()
     }
 
     private fun fuelForWeight(weight: Int): Int {
         return weight / 3 - 2
+    }
+
+    private fun fuelForFuel(fuel: Int): Int {
+        var neededFuel = 0
+        var partialFuel = fuelForWeight(fuel)
+        while (partialFuel > 0) {
+            neededFuel += partialFuel
+            partialFuel = fuelForWeight(partialFuel)
+        }
+        return neededFuel
     }
 }
