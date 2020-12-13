0997419d69e0d954afa2eea57eec5f6093a60818
kmakma <michaelo1@gmx.de>
2020-12-13 08:45:45 +0100

    solved: 2020.13

--- src/main/kotlin/io/github/kmakma/adventofcode/y2020/Y2020Day13.kt
+++ src/main/kotlin/io/github/kmakma/adventofcode/y2020/Y2020Day13.kt
@@ -1,16 +1,13 @@
 package io.github.kmakma.adventofcode.y2020
 
 import io.github.kmakma.adventofcode.utils.Day
+import io.github.kmakma.adventofcode.utils.lcm
 
-suspend fun main() {
+fun main() {
     Y2020Day13().solveAndPrint()
-//    Y2020Day13().apply {
-//        initializeDay()
-//        solveTask2()
-//    }
 }
 
-class Y2020Day13 : Day(2020, 13, "") {
+class Y2020Day13 : Day(2020, 13, "Shuttle Search") {
 
     private lateinit var busses: List<Int>
     private lateinit var inputList: List<String>
@@ -30,66 +27,21 @@ class Y2020Day13 : Day(2020, 13, "") {
     }
 
     override suspend fun solveTask2(): Any? {
-        val lastIndex = inputList.lastIndex
         val departs = inputList.last().split(",").mapIndexedNotNull { index, busId ->
-            if (busId == "x") null else BusTimestamp(basis = busId.toInt(), shift = lastIndex - index)
+            if (busId == "x") null else Pair(index, busId.toLong())
         }
-        println("depart: check; $departs")
-        var max = departs.maxOrNull()!!.value()
-        println("max: check; $max")
-        while (departs.notFinished()) {
-            departs.forEach { current ->
-                if (current.value() < max) {
-                    max = current.adjustTo(max)
-                }
-            }
-//            println("new max: $max")
-        }
-        println("new max: $max")
-        return departs.first().value()
-        TODO("Not yet implemented")
-    }
+        // solution by Oggiva: https://www.reddit.com/r/adventofcode/comments/kc4njx/2020_day_13_solutions/gfnno67/?context=3
+        var step = departs.first().second
+        var nextBus = 1
+        var timestamp = 0L
+        do {
+            timestamp += step
+            if ((timestamp + departs[nextBus].first) % departs[nextBus].second == 0L)
+                step = lcm(step, departs[nextBus++].second)
+        } while (nextBus < departs.size)
+        println(timestamp)
 
-    private fun lcmShifted(a: Long, b: Long) {
-        var x = 1
-        var y = 1
-        var ax = a * x + 1
-        var by = b * y
-        while (ax != by) {
-            if (ax < by) {
-                x++
-                ax = a * x + 1
-            } else if (ax >= by) {
-                y++
-                by = b * y
-            }
-        }
-        println("${x * a} ${b * y}")
-    }
-}
-
-private fun List<BusTimestamp>.notFinished(): Boolean {
-    var previous = first()
-    forEach { current ->
-        if (previous.value() != current.value()) return true
-        previous = current
-    }
-    return false
-}
-
-private data class BusTimestamp(val basis: Int, var factor: Long = 7692307692307L, val shift: Int) :
-    Comparable<BusTimestamp> {
-
-    fun value() = basis * factor + shift
-
-    override fun compareTo(other: BusTimestamp) = value().compareTo(other.value())
-
-    fun adjustTo(target: Long): Long {
-        if (value() < target) {
-            val x = if ((target - shift) % basis == 0L) 0L else 1L
-            factor = (target - shift) / basis + x
-        }
-        return value()
+        return timestamp
     }
 
 }
