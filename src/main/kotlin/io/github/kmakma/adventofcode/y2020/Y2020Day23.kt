92be3520ef735963d945b7617ab5fb9f6f8bedfa
kmakma <michaelo1@gmx.de>
2020-12-22 08:04:25 +0100

    framework: 2020.23-.25

---
+++ src/main/kotlin/io/github/kmakma/adventofcode/y2020/Y2020Day23.kt
@@ -0,0 +1,23 @@
+package io.github.kmakma.adventofcode.y2020
+
+import io.github.kmakma.adventofcode.utils.Day
+
+fun main() {
+    Y2020Day23().solveAndPrint()
+}
+
+class Y2020Day23 : Day(2020, 23, "???") {
+    private lateinit var inputLines: List<String>
+
+    override fun initializeDay() {
+        inputLines = inputInStringLines()
+    }
+
+    override suspend fun solveTask1(): Any? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun solveTask2(): Any? {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/main/kotlin/io/github/kmakma/adventofcode/y2020/Y2020Day25.kt
@@ -0,0 +1,23 @@
+package io.github.kmakma.adventofcode.y2020
+
+import io.github.kmakma.adventofcode.utils.Day
+
+fun main() {
+    Y2020Day25().solveAndPrint()
+}
+
+class Y2020Day25 : Day(2020, 25, "???") {
+    private lateinit var inputLines: List<String>
+
+    override fun initializeDay() {
+        inputLines = inputInStringLines()
+    }
+
+    override suspend fun solveTask1(): Any? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun solveTask2(): Any? {
+        TODO("Not yet implemented")
+    }
+}
